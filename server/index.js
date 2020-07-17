8e54e06e986115dd3b65b8da448b4bd2ae172b71
“Gurveer <gurveerdhindsa@hotmail.com>
2020-07-17 10:43:44 -0400

    Initial server code

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+const functions = require('firebase-functions');
+
+const app = express();
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/v1/test', (req, res) => {
+  res.send('Called /v1/test');
+})
+
+const api = functions.https.onRequest(app);
+
+module.exports = {
+  api,
+};
