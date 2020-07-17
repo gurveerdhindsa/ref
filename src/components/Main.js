95299b031793cef846bd03d0b6d583b61bbda4b1
“Gurveer <gurveerdhindsa@hotmail.com>
2020-07-17 14:37:58 -0400

    Display some placeholder content

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,15 @@ import '../assets/css/main.less';
 export default class Main extends Component {
   render() {
     return (
-        <div className="main">
-          <h1>Welcome!</h1>
+      <div className="main">
+        <h1>Welcome!</h1>
+        <h4>React-Express-Firebase Boilerplate</h4>
+
+        <div className="content">
+          <p>Here is a picture of a dog</p>
+          <img src="images/dog.jpg" alt="happy dog" />
         </div>
+      </div>
     );
   }
 }
