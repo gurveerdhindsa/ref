131ecd52d09d8a54d28ac43836994f558e70e130
“Gurveer <gurveerdhindsa@hotmail.com>
2020-07-17 10:31:39 -0400

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import '../assets/css/main.less';
+
+export default class Main extends Component {
+  render() {
+    return (
+        <div className="main">
+          <h1>Welcome!</h1>
+        </div>
+    );
+  }
+}
