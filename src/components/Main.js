509adcc052aa08894d5b921004fb750633dbaaba
“Gurveer <gurveerdhindsa@hotmail.com>
2020-07-27 14:14:36 -0400

    Make a sample request to backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,27 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import '../assets/css/main.less';
 
-export default class Main extends Component {
-  render() {
-    return (
-      <div className="main">
-        <div className="content">
-          <h1>React-Express-Firebase Boilerplate</h1>
-          <p>Here is a picture of my dog</p>
-          <img
-            className="content-image--dog"
-            src="images/dog.jpg"
-            alt="happy dog"
-          />
-        </div>
+const Main = () => {
+  useEffect(() => {
+    fetch('/v1/test')
+      .then((response) => response.text())
+      .then((text) => console.log(text));
+  });
+
+  return (
+    <div className="main">
+      <div className="content">
+        <h1>React-Express-Firebase Boilerplate</h1>
+        <p>Here is a picture of my dog</p>
+        <img
+          className="content-image--dog"
+          src="images/dog.jpg"
+          alt="happy dog"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Main;
